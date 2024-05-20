dd347ad77e9a53fa821d8593f1ac1fecdee328d7
Lautaro Maruniak <lauti083@gmail.com>
2024-05-20 00:18:59 -0300

    Subi el token reset

--- src/utils/tokenreset.js
+++ src/utils/tokenreset.js
@@ -2,7 +2,6 @@ function generateResetToken() {
     const token = Math.floor(Math.random() * (999999 - 100000 + 1)) + 100000;
     return token.toString();
 }
-
 module.exports = {
     generateResetToken
 };
